4292fc7e46cffde86d708903e237fba90e1feb64
Nathan Amin <nathanamin@gmail.com>
2023-03-27 18:49:59 -0700

    add HelloApiHandler

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS #comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+CORS(app) #comment this on deployment
+api = Api(app)
 
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
 
-@app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+api.add_resource(HelloApiHandler, '/flask/hello')
