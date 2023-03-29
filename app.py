0015280ba89273142e177223fdddb63f04d2864c
Nathan Amin <nathanamin@gmail.com>
2023-03-28 23:27:25 -0700

    comment CORS import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
+# from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
